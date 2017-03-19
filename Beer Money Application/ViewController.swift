caf8571a1d66ba47f47c67ff362385a10fd25326
Kevin Connor <kevinconnor@Kevins-MacBook-Pro.local>
2017-03-19 10:49:50 -0500

    This is the first revision. Started by adding buttons and getting current title from the buttons. Next need to perform action with that title.

--- Beer Money Application/ViewController.swift
+++ Beer Money Application/ViewController.swift
@@ -10,17 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        print("I can't wait to push this version to github")
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func beerValueButton(_ sender: UIButton) {
+        
+        let beerCostButton = sender.currentTitle!
+        
+        
+        
     }
-
 
 }
 
